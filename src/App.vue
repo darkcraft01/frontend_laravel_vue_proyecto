b8cc219936577e519313f40687be1c315b9bf361
Oliver <oliverbolyugi@gmail.com>
2025-04-02 15:34:39 -0400

    proyecto Vue

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template>
+    <h1>App.vue</h1>
+
+    <RouterLink to="/">INICIO</RouterLink> |
+    <RouterLink to="/nosotros">Nosotros</RouterLink> |
+    <RouterLink to="/servicios">Servicios</RouterLink> |
+    <RouterLink to="/blog">Blog</RouterLink> |
+    <RouterLink to="/auth/login">Ingresar</RouterLink>
+    <br>
+
+    <RouterView />
+    
+</template>
+
+
---
+++ src/views/auth/Login.vue
@@ -0,0 +1,29 @@
+<template>
+    <h1>Ingresar</h1>
+
+    <label for="">Ingrese Correo:</label>
+    <input type="email" v-model="credenciales.email">
+    <br>
+    <label for="">Ingrese Contraseña</label>
+    <input type="password" v-model="credenciales.password">
+    <br>
+    <button @click="funIngresar()">Ingresar</button>
+    <br>
+    {{ credenciales }}
+    <br>
+    {{ respuesta }}
+
+</template>
+<script setup>
+import { ref } from 'vue';
+import authService from '../../services/auth.service';
+
+const credenciales = ref({email: "", password: ""});
+const respuesta = ref();
+
+async function funIngresar(){
+    const res = await authService.login(credenciales.value);
+
+    respuesta.value = res.data
+}
+</script>
